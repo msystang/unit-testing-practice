d8761c2f7cd09b69915768dff768c882b4f0fe82
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-30 15:54:01 -0400

    adds tableview details to JokeListVC

--- unit-testing-practice/unit-testing-practice/Controllers/JokeListViewController.swift
+++ unit-testing-practice/unit-testing-practice/Controllers/JokeListViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class JokeListViewController: UIViewController {
 
+    var jokes = [Joke]()
+    @IBOutlet weak var jokeListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
     }
 
+    func configureTableView() {
+        jokeListTableView.delegate = self
+        jokeListTableView.dataSource = self
+    }
 
 }
 
+extension JokeListViewController: UITableViewDelegate {}
+
+extension JokeListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jokes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = jokeListTableView.dequeueReusableCell(withIdentifier: "jokeCell", for: indexPath)
+        let joke = jokes[indexPath.row]
+        
+        cell.textLabel?.text = joke.setup
+        
+        return cell
+    }
+    
+    
+}
