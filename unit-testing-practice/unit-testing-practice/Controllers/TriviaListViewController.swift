1f5fc5ac931327464ad3d823ab571a10b90cdca0
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-30 16:35:58 -0400

    adds details to TriviaListVC

--- unit-testing-practice/unit-testing-practice/Controllers/TriviaListViewController.swift
+++ unit-testing-practice/unit-testing-practice/Controllers/TriviaListViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class TriviaListViewController: UIViewController {
     
+    var allTrivia = [Trivia]()
+    
+    @IBOutlet weak var triviaListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
+        loadTriviaData()
+        
+    }
+    
+    private func configureTableView() {
+        triviaListTableView.delegate = self
+        triviaListTableView.dataSource = self
+    }
+    
+    private func loadTriviaData() {
+        let triviaData = DataFetchingService.getTriviaDataFromJSON()
+        // refactor later to not force unwrap, error enum?
+        allTrivia = TriviaWrapper.getAllTrivia(from: triviaData)!.trivia
+    }
+    
+}
+
+extension TriviaListViewController: UITableViewDelegate {}
+
+extension TriviaListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTrivia.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = triviaListTableView.dequeueReusableCell(withIdentifier: "triviaCell", for: indexPath)
+        let trivia = allTrivia[indexPath.row]
+        
+        cell.textLabel?.text = trivia.question
+        
+        return cell
     }
     
     
 }
-
