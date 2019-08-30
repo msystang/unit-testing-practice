cbf988288131ad12a2da8aace291768ef38e489a
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-30 16:29:20 -0400

    adds details to MovieListVC

--- unit-testing-practice/unit-testing-practice/Controllers/MovieListViewController.swift
+++ unit-testing-practice/unit-testing-practice/Controllers/MovieListViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class MovieListViewController: UIViewController {
     
+    var movies = [Movie]()
+    
+    @IBOutlet weak var movieListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
+        loadMovieData()
+        
+    }
+    
+    private func configureTableView() {
+        movieListTableView.delegate = self
+        movieListTableView.dataSource = self
+    }
+    
+    private func loadMovieData() {
+        let moviesData = DataFetchingService.getMoviesDataFromJSON()
+        // refactor later to not force unwrap, error enum?
+        movies = MovieWrapper.getAllMovies(from: moviesData)!.movie
+    }
+    
+}
+
+extension MovieListViewController: UITableViewDelegate {}
+
+extension MovieListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = movieListTableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        let movie = movies[indexPath.row]
+        
+        cell.textLabel?.text = movie.title
+        
+        return cell
     }
     
     
 }
-
